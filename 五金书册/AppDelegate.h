0e4642191c49ec49d52adac91b84c62f3cd6a1b2
NovaDev <novadev@163.com>
2015-09-30 22:18:30 +0800

    Initial Commit

---
+++ 五金书册/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  五金书册
+//
+//  Created by 李庆阳 on 15/9/30.
+//  Copyright © 2015年 NovaDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 五金书册/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  五金书册
+//
+//  Created by 李庆阳 on 15/9/30.
+//  Copyright © 2015年 NovaDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
